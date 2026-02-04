cbc56b3f4bbec40dc4f9b7661d9c7cf052a96756
Your Name <you@example.com>
2026-02-04 11:31:24 +0700

    feat:

--- obj/Debug/net8.0/beresbackend.AssemblyInfo.cs
+++ obj/Debug/net8.0/beresbackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("beresbackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f689785d26f8a895affe37c2834d02a94d790f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab12101e4a160a424a50e70ec5c2379a1af06900")]
 [assembly: System.Reflection.AssemblyProductAttribute("beresbackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("beresbackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
